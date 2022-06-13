48dc5fbf82d6dc6c161d5effc909c147f2b68e88
imtgs <imtgs@qq.com>
2022-06-14 06:43:59 +0800

    Site updated: 2022-06-14 06:43:59

--- js/welcome.js
+++ js/welcome.js
@@ -1,5 +1,5 @@
 function welcome(){
-  let welcome_text = '欢迎光顾本蒟蒻的小窝~'
+  let welcome_text = '欢迎光顾XShaw的博客~'
   if(document.referrer!==''){
     let referrer=document.referrer.split("/")[2];
     welcome_text="欢迎你，来自"+referrer.toUpperCase()+"的用户！";
@@ -7,8 +7,8 @@ function welcome(){
   }
   swal({
     title: " 欢迎！",
-    text: welcome_text+'\n打开页面下方音乐以获得更佳体验！',
-    imageUrl: "/img/avatar.jpg",
+    text: welcome_text,
+    imageUrl: "https://s2.loli.net/2022/05/08/cQ48WlKO3NuoESs.jpg",
     timer: 3000,
     showConfirmButton: false
   });
